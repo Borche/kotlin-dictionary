9dcd99a51d327488f77427a1a42275ff6355e29a
Andreas Börjesson <borjesson.andreas@gmail.com>
2021-04-04 18:53:32 +0200

    Added faunaDB

--- src/main/kotlin/com/ab/ploy/PloyApplication.kt
+++ src/main/kotlin/com/ab/ploy/PloyApplication.kt
@@ -1,20 +1,64 @@
 package com.ab.ploy
 
+import com.ab.ploy.models.Beer
+import com.faunadb.client.FaunaClient
+import com.faunadb.client.query.Language.*
+import com.faunadb.client.types.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.CompletableFuture
 
 @SpringBootApplication
 @Controller
 class PloyApplication {
 
-    @GetMapping("/")
+    @org.springframework.beans.factory.annotation.Value("\${fauna-db.secret}")
+    lateinit var faunaDbSecret: String
+
+    @PostMapping("/beers")
     @ResponseBody
-    fun helloWorld(): String {
-        return "hello world";
+    fun helloWorld(@RequestBody beer: Beer): ResponseEntity<Beer> {
+        val client = FaunaClient.builder()
+            .withSecret(faunaDbSecret)
+            .build();
+
+        val result: Value = client.query(
+            Create(
+                Collection("beers"),
+                Obj("data", Value(beer))
+            )
+        ).get()
+
+        val beer3: Beer = result.at("data").to(Beer::class.java).get()
+
+        return ResponseEntity.ok(beer3)
+    }
+
+    fun createBeers() {
+        val client = FaunaClient.builder()
+            .withSecret("fnAEF85GlIACAcQc4a-t5MmBVj6v9S7b5XDgpzKJ")
+            .build();
+
+        val completableFuture = client.query(
+            Map(
+                Value(listOf<String>("Budweiser", "Sofiero")),
+                Lambda(
+                    Value("beer"),
+                    Create(
+                        Collection("beers"),
+                        Obj("data",
+                            Obj(
+                                "name", Var("beer"),
+                                "alcohol", Value("5%")
+                            )
+                        )
+                    )
+                )
+            )
+        )
     }
 }
 
