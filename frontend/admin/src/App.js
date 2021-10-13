82501d0f42c3337314abf7f56ea97bf1abf96069
Andreas Börjesson <borjesson.andreas@gmail.com>
2021-10-13 23:05:48 +0200

    Just changed the React app a lil' bit to get going. Very basic.

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -1,33 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import {Component} from "react";
+import { Component } from 'react';
+import Header from './components/header/header.component';
 
 class App extends Component {
-
   async componentDidMount() {
-    const response = await fetch("/admin/api/words");
+    const response = await fetch('/admin/api/words');
     const body = await response.json();
     console.log(body);
   }
 
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+      <div className="wrapper">
+        <Header></Header>
+      </div>
     );
   }
 }
