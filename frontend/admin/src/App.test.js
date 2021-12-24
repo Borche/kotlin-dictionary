386a6504a993d41133a00066e8565a756a80c740
Andreas Börjesson <borjesson.andreas@gmail.com>
2021-12-24 11:01:30 +0100

    Fixed FE test by importing the real redux store.

--- frontend/admin/src/App.test.js
+++ frontend/admin/src/App.test.js
@@ -1,12 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { Provider } from "react-redux";
-
-const lawl = {};
+import { store } from "./redux/store";
 
 test("renders learn react link", () => {
   const { container } = render(
-    <Provider store={lawl}>
+    <Provider store={store}>
       <App />
     </Provider>
   );
